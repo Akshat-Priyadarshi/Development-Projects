5aea2053f69a4c32a7407bb4a1439342fcf2291c
Akshat-Priyadarshi <workspace.asap@gmail.com>
2025-01-30 10:41:47 +0530

    Update booking_services.dart

--- booking_services.dart
+++ booking_services.dart
@@ -1,1 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/models/booking.dart';
 
+class BookingService {
+  final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Stream<List<Booking>> getBookings() {
+    return _firestore.collection('bookings').snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return Booking.fromMap(doc.data() as Map<String, dynamic>);
+      }).toList();
+    });
+  }
+}
