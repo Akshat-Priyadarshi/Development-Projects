36fbff8694f5c2476fdde9bad88e3eb3d6dc1d78
Akshat-Priyadarshi <workspace.asap@gmail.com>
2025-01-30 10:43:48 +0530

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/models/booking.dart';
+import 'package:flutter_application_1/widgets/calender.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Guest Room Booking'),
+      ),
+      body: BookingCalendar(
+        bookings: Provider.of<List>(context),
+        onDaySelected: (date) {
+          // Handle date selection
+        },
+      ),
+    );
+  }
+}
 
