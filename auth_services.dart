8d7ee83f7e1bcfe7d866c5b8259df9b9928ef06c
Akshat-Priyadarshi <workspace.asap@gmail.com>
2025-01-30 10:42:50 +0530

    Update auth_services.dart

--- auth_services.dart
+++ auth_services.dart
@@ -1,1 +1,39 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logger/logger.dart';
 
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final Logger _logger = Logger();
+
+  Future<User?> signInWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    } catch (e) {
+      _logger.e("Sign in failed", error: e);
+      return null;
+    }
+  }
+
+  Future<User?> registerWithEmailPassword(String email, String password) async {
+    try {
+      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
+      return result.user;
+    } catch (e) {
+      _logger.e("Registration failed", error: e);
+      return null;
+    }
+  }
+
+  Future signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      _logger.e("Sign out failed", error: e);
+    }
+  }
+
+  Stream<User?> get user {
+    return _auth.authStateChanges();
+  }
+}
